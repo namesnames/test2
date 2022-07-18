a19aca723312ea597e3a8bfa1ed09bc1731bc083
97gkswn <97gkswn@naver.com>
2022-07-18 21:39:36 +0900

    feat:signup&login

--- popit_project/accounts/views.py
+++ popit_project/accounts/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import APIView
+from .serializer import RegisterSerializer,UserSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import status
+from django.contrib.auth import authenticate
 
-# Create your views here.
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            # jwt token 접근해주기
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "register successs",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            #쿠키에 넣어주기...아직 어떤식으로 해야될지 모르겠는데 이렇게 설정만 우선 해주었다. 
+            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("refresh", refresh_token, httponly=True)
+            return res
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class AuthView(APIView):
+    
+    def post(self, request):
+        user = authenticate(
+            login_id=request.data.get("login_id"), password=request.data.get("password")
+        )
+        if user is not None:
+            serializer = UserSerializer(user)
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "login success",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+            return res
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)        
