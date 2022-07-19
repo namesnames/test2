4f9385066ded15bc63a97686064a5bd9bd835be4
msung99 <msung6924@naver.com>
2022-07-19 20:22:23 +0900

    feat:signup&login

--- popit_project/accounts/models.py
+++ popit_project/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
+
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
@@ -28,15 +29,60 @@ class UserManager(BaseUserManager):
         superuser.save(using=self._db)
         return superuser
 
+# User 는 깡통이다. (아무 기능이 없음) => AbstractBaseUser 를 반드시 상속받아야함
+# 대부분의 속성은 User 가 아닌, User 가 상속받는 AbstractUser 가 다 가지고있다.
+# 즉, User 는 기능이 없는 깡동 수준이고, 장고 내부에 세팅된 값이라 값도 변경 불가능하다. 따라서 User 에 대한 재정의(오버라이딩)이 필요하다!
+
+# User 에 대한 재정의를 다 마치고나면, settings.py 에 AUTH_USER_MODEL 에 "AUTH_USER_MODEL = accounts.User" 와 같이 추가해야함
+# "AUTH_USER_MODEL" 은 accounts 앱의 models.py 에서 내가 재정의한 User 를 사용하겠다고 설정하는 부분이다.
+# (AUTH_USER_MODEL 의 디폴트 값 => AUTH_USER_MODEL = 'auth.User')
+
+# get_user_model() : 기본적으로 깡통 User 를 가리킴. 그런데 개발자가 재정의하면, 사용자 재정의 User 를 가리키게 됨
+
+# get_user_model() VS settings.AUTH_USER_MODEL (차이점)
+# get_user_model() : 클래스 // settings.AUTH_USER_MODEL : 문자열
+
+'''
+get_user_model()은 아래와 같이 문자열이 아닌 클래스가 들어가야 하는 경우에는 사용해 주면 된다.
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+# 그대로 활용하지 못하는 경우는 항상 상속받아서 custom한다.
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'last_name', 'email']
+'''
+
+'''
+아래와 같이 클래스 내부에 들어가는 경우는 문자열을 쓰는 것이 맞다.
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    followers = models.ManyToManyField(
+            settings.AUTH_USER_MODEL,
+            related_name='followings'
+        )
+'''
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     login_id = models.CharField(max_length=30, unique=True, null=False, blank=False)
     email = models.EmailField(max_length=30, unique=True, null=False, blank=False)
+    password = models.CharField(default = "비밀번호 입력", max_length = 12)
+    re_password = models.CharField(default = "비밀번호 재확인", max_length = 12)
+    promise = models.BooleanField(default = False) # 약관동의 
+    alarm = models.BooleanField(default = True) # 알람 디폴트 값 = ON
+
+    '''
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    '''
 
     objects = UserManager()
 
--- popit_project/accounts/views.py
+++ popit_project/accounts/views.py
@@ -27,7 +27,7 @@ class RegisterAPIView(APIView):
                 status=status.HTTP_200_OK,
             )
             #쿠키에 넣어주기...아직 어떤식으로 해야될지 모르겠는데 이렇게 설정만 우선 해주었다. 
-            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("access", access_token, httponly=True) # 유저가 로그인에 성공했을때, 해당 유저를 로그인 기간동안 인증하는 토큰을 cookie에 저장해야 할 경우, HttpResponse 또는 JsonResponse 에 장고에서 제공하는 set_cookie 메소드를 사용하여 토큰을 발행할 수 있다.
             res.set_cookie("refresh", refresh_token, httponly=True)
             return res
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
