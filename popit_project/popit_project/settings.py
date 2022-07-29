b42f3fe6aec665a008aefd4c23729b4d7c5c81f8
97gkswn <97gkswn@naver.com>
2022-07-30 03:03:26 +0900

    hi2

--- popit_project/popit_project/settings.py
+++ popit_project/popit_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@(d-*8h#%#&3!on^m2kor%wjecr0z!_12_fobm02zqmbv8()l-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
